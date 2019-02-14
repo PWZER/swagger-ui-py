4f6f87ca2d008ef8359898e13ac9397e9b3cd2d8
klaashoekstra94 <klaashoekstra94@users.noreply.github.com>
2019-02-14 11:43:24 +0100

    Added aiohttp_swagger_ui to __init__

--- src/swagger_ui/__init__.py
+++ src/swagger_ui/__init__.py
@@ -3,6 +3,7 @@ import sys
 if sys.version_info >= (3, 0):
     from .py3.sanic_swagger_ui import api_doc as sanic_api_doc
     from .py3.quart_swagger_ui import api_doc as quart_api_doc
+    from .py3.aiohttp_swagger_ui import api_doc as aiohttp_api_doc
 
 from .flask_swagger_ui import api_doc as flask_api_doc
 from .tornado_swagger_ui import api_doc as tornado_api_doc
