ad32f653deefdcc9f66aceb418cc6c4ad67bfdf2
klaashoekstra94 <klaashoekstra94@users.noreply.github.com>
2019-02-14 11:49:39 +0100

    Added example for aiohttp

---
+++ examples/aiohttp_test.py
@@ -0,0 +1,19 @@
+import os
+from aiohttp import web
+
+
+async def hello(request):
+    return web.Response(text="Hello, world")
+
+
+app = web.Application()
+app.add_routes([web.get('/hello/world', hello)])
+
+
+if __name__ == '__main__':
+    working_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(working_dir, 'conf/test.yaml')
+
+    from swagger_ui import aiohttp_api_doc
+    aiohttp_api_doc(app, config_path=config_path)
+    web.run_app(app)
