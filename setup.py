108bd3923c312a080f09fa9480f010e39631d02f
Jakub Havlíček <jakub.havlicek@ceskatelevize.cz>
2020-07-13 17:43:43 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def load_requirements():
 if __name__ == '__main__':
     setup(
         name='swagger-ui-py',
-        version='0.3.0',
+        version='0.3.1',
         description=DESCRIPTION,
         long_description=readme(),
         long_description_content_type='text/markdown',
